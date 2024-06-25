3bad9b59e364dc5f94b200db342dfec1d05dfd1b
Emiliospeed <121364719+Emiliospeed@users.noreply.github.com>
2024-06-25 11:44:45 +0300

    Create compiler.c

---
+++ compiler.c
@@ -0,0 +1,12 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+//Constants Definitions
+#define MEMORY_SIZE 4096
+#define MAX_LINE_LENGTH 500 
+#define MAX_LABEL_LENGTH 50
